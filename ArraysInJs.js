1beb32f53f117a37879f0d2f10648ab76ff5519e
jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2026-08-06 12:31:11 +0100

    Functions and advanced array lesson

---
+++ ArraysInJs.js
@@ -0,0 +1,134 @@
+//------------------------------------------------------------------------------------
+// --------------------------------ARRAYS IN JAVASCRIPT-------------------------------
+//------------------------------------------------------------------------------------
+/*Arrays are ordered collections of values that can hold multiple data types. They are used to store and manipulate lists of data. In JavaScript, arrays are created using square brackets [] and can contain any type of data, including other arrays and objects.
+Example of an array:
+let fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+*/
+
+let fruits = [
+  "Apple", //0
+  "Banana", //1
+  "Cherry", //2
+  "Date", //3
+  "Elderberry", //4
+];
+//-----------------------------------------
+// how to access an array: arrayCollection[]
+
+// console.log("The first fruit is: " + fruits[5]); // accessing the first element
+
+// ----------------------------------
+// checking the length of the array
+// console.log("The number of fruits in the array is: " + fruits.length); // checking the length of the array
+// fruits[0] = "Mango"; // changing the first element of the array
+
+//methods of arrays in js
+/*
+  Some common array methods include:
+  - push(): Adds one or more elements to the end of an array.
+  - pop(): Removes the last element from an array.
+  - shift(): Removes the first element from an array.
+  - unshift(): Adds one or more elements to the beginning of an array.
+  - splice(): Adds or removes elements from an array at a specified index.
+*/
+
+// example of push() method
+// fruits.push("Fig");
+// console.log("After push, the fruits array is: " + fruits);
+
+// //example of pop() method
+// fruits.pop();
+// console.log("After pop, the fruits array is: " + fruits);
+
+// //example of shift() method
+// fruits.shift();
+// console.log("After shift, the fruits array is: " + fruits);
+
+// //example of unshift() method
+// fruits.unshift("Grapes");
+// console.log("After unshift, the fruits array is: " + fruits);
+
+// //example of splice() method
+// fruits.splice(2, 1, "Kiwi"); // at index 2, remove 1 element and add "Kiwi"
+// console.log("After splice, the fruits array is: " + fruits);
+
+// ---------------------------------------------------------------------------------------
+
+/*
+ iteration of an array; 
+ foreach loop
+    The forEach() method executes a provided function once for each array element.
+*/
+
+let arrayExample = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 25 },
+  { name: "Mike", age: 35 },
+  { name: "Sarah", age: 28 },
+  { name: "Tom", age: 22 },
+  { name: "Emily", age: 27 },
+];
+
+arrayExample.forEach((val, index) => {
+  console.log(
+    val.name + " is " + val.age + " years old.\nAnd the index is " + index,
+  );
+});
+
+/*
+Transformation
+    The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+*/
+// let fruits = ["Apple", "Banana", "Orange"];
+// console.log(fruits);
+
+// const transformedArray = fruits.map((fruit, i) => fruit?.toUpperCase());
+// console.log(transformedArray);
+
+/*
+Filter
+    filter in javascript creates a new array with result of the function called of filtered through
+*/
+
+// let userData = [
+//   { name: "Jonathan", age: 30, isMarried: false },
+//   { name: "Jane", age: 25, isMarried: true },
+//   { name: "Mike", age: 35, isMarried: false },
+//   { name: "Sarah", age: 28, isMarried: true },
+//   { name: "Tom", age: 22, isMarried: false },
+//   { name: "Emily", age: 27, isMarried: true },
+//   { name: "David", age: 32, isMarried: false },
+//   { name: "Sophia", age: 29, isMarried: true },
+//   { name: "James", age: 31, isMarried: false },
+//   { name: "Olivia", age: 26, isMarried: true },
+//   { name: "Jonathan", age: 39, isMarried: true },
+// ];
+
+// let filteredData = userData.filter(
+//   (user) => user.isMarried === true && user.age < 29 && user.name.startsWith("J")
+// );
+
+// console.log(filteredData);
+
+/*
+Searching
+  find- searches through the array and returns the first element that satisfies the provided testing function.
+  some- tests whether at least one element in the array passes the test implemented by the provided function.
+*/
+// let findvalue = userData.find((user) => user.name === "Jonathan");
+// let isValue = userData.some((user) => user.name === "Jonathan");
+
+// console.log(findvalue);
+// console.log(isValue); // returns true if the object is found in the array
+
+// userData.includes({ name: "Jonathan", age: 30, isMarried: false }); // returns true if the object is found in the array
+
+// delete findvalue["age"];
+// findvalue.height = "6ft";
+
+// console.log(findvalue);
+
+// findvalue.name = "Davido";
+
+// console.log(findvalue);
--- JSFunctions.js
+++ JSFunctions.js
@@ -33,12 +33,52 @@ const SI = function (P, R, T) {
   alert(`This is the result: ${result}`);
 };
 
-SI(500000, 25, 4);
+// SI(500000, 25, 4);
 
 const AOR = (L, B) => {
   const result = L * B;
   alert(`The area of a rectangle is: ${result}`);
 };
+// AOR(60, 40);
 
+// Function examples
+const quadraticEqu = (a, b, c) => {
+  const positiveResult = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a);
 
-AOR(60, 40)
+  const nagetiveResult = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a);
+
+  alert(
+    `the positive result is: ${positiveResult}\nWhile the nagative result is: ${nagetiveResult}`,
+  );
+};
+
+// quadraticEqu(1, 7, 12);
+
+const AOT = function (a, b) {
+  const result = (a * b) / 2;
+
+  alert("The area of a triange is: " + result);
+};
+
+// AOT(5, 25);
+
+// // a callback function is a function that is passed as an argument to another function and is executed after some operation has been completed. It allows you to handle asynchronous operations and execute code after a certain event has occurred.
+// // Example of a callback function
+function fetchData(A, B) {
+  setTimeout(() => {
+    // const data = "Data fetched from the server";
+    // alert(data); // calling the callback function with the fetched data
+    AOT(A, B);
+  }, 2000); // simulating a delay of 2 seconds
+}
+
+// fetchData(5, 25);
+
+// setTimeout(() => {
+//   alert("This message is being displayed after 5sec")
+// }, 5000);
+
+// setTimeout(() => {
+  
+// }, timeout);
+
